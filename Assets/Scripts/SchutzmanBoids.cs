6b1574a9724d678f516a4c63f43cefcfa762dea0
Ethan Schutzman <ehschutzman@wpi.edu>
2017-12-11 20:48:06 -0500

    Changed fields in SchutzmanBoid

--- Assets/Scripts/SchutzmanBoids.cs
+++ Assets/Scripts/SchutzmanBoids.cs
@@ -4,181 +4,89 @@ using UnityEngine;
 
 public class SchutzmanBoids : MonoBehaviour
 {
-	private float separation;
-	private float flocking;
-	private float cohesion;
-	private float direction;
-	private float COH_NEIGHBORDIST = 50;  //radius for boids to be cohesive with
-	private float ALI_NEIGHBORDIST = 50;  //radius for boids to align with
-	private float SEP_DESIREDDIST = 20;   //desired separation distance
-	private float angle;
+	 float separation;
+	 float flocking;
+	 float cohesion;
+	 float direction;
+	 float COH_NEIGHBORDIST = 50;  //radius for boids to be cohesive with
+	 float ALI_NEIGHBORDIST = 50;  //radius for boids to align with
+	 float SEP_DESIREDDIST = 20;   //desired separation distance
+	 float angle;
+	
+	 float mass;
+	 float strength;
+	 float size;
+	 float agression;
+	 float turnSpeed;
+	 float bounciness;
+	 int shape;
+	float fear;
 	
 	
 	
-	private float speed;
-	private float mass;
-	private float strength;
-	private float size;
-	private float agression;
-	private float turnSpeed;
-	private float bounciness;
-	private int shape;
-	private float fear;
+	public GameObject boid;
+    public GameObject target;
+    float speed;
+    public Vector3 center;
+    //Collider m_collider;
+    private Rigidbody rbody;
+    int count;
 	
+	
+    // Use this for initialization
+    // Use this for initialization
+    void Start () {
+        boid = GameObject.Find("BasicBoid");
+        speed = 2f;
+        count = 0;
+        center = new Vector3(0, 0, 0);
+        rbody = GetComponent<Rigidbody>();
+        //m_collider = GetComponent<Collider>();
+        //m_collider.enabled = true;
+    }
 
-	// Use this for initialization
-	void Start ()
-	{
-		Transform[] array;
-
-	}
-
-	// Update is called once per frame
-	void Update ()
-	{
+    // Update is called once per frame
+    void Update () {
+        count++;
+        float step = speed * Time.deltaTime;
+        var heading = center - transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+        //transform.position = Vector3.MoveTowards(transform.position, boid.transform.position, step);
+        //gameObject.transform.position += Vector3.forward;
+        //float distance = Vector3.Distance(this.transform.position, boid.transform.position);
+        //gameObject.transform.position += Vector3.MoveTowards(transform.position, boid.transform.position, step);
+        //rbody.AddForce(20,0,20);
+        //rbody.AddForce(direction * 10);
 		
-		
-		
-	}
+        if (distance > 0)
+        {
+            direction = heading / distance;
+            rbody.AddForce(direction * 10);
+        }
 
-
-	void ExplosionDamage(Vector3 center, float radius)
-	{
-		
-		Collider[] hitColliders = Physics.OverlapSphere(center, radius);
-		int i = 0;
-		while (i < hitColliders.Length)
-		{
-			hitColliders[i].SendMessage("AddDamage");
-			i++;
-		}
-	}
-	
-	
-	
-	
-	
-	public void init(float separation, float flocking, float cohesion, float direction, float angle, float speed, float mass, float strength, float size, float agression, float turnSpeed, float bounciness, int shape, float fear)
-	{
-		this.separation = separation;
-		this.flocking = flocking;
-		this.cohesion = cohesion;
-		this.direction = direction;
-		this.angle = angle;
-		this.speed = speed;
-		this.mass = mass;
-		this.strength = strength;
-		this.size = size;
-		this.agression = agression;
-		this.turnSpeed = turnSpeed;
-		this.bounciness = bounciness;
-		this.shape = shape;
-		this.fear = fear;
-	}
-
-	public float CohNeighbordist
-	{
-		get { return COH_NEIGHBORDIST; }
-		set { COH_NEIGHBORDIST = value; }
-	}
-
-	public float AliNeighbordist
-	{
-		get { return ALI_NEIGHBORDIST; }
-		set { ALI_NEIGHBORDIST = value; }
-	}
-
-	public float SepDesireddist
-	{
-		get { return SEP_DESIREDDIST; }
-		set { SEP_DESIREDDIST = value; }
-	}
-
-	public float Separation
-	{
-		get { return separation; }
-		set { separation = value; }
-	}
-
-	public float Flocking
-	{
-		get { return flocking; }
-		set { flocking = value; }
-	}
-
-	public float Cohesion
-	{
-		get { return cohesion; }
-		set { cohesion = value; }
-	}
-
-	public float Direction
-	{
-		get { return direction; }
-		set { direction = value; }
-	}
-
-	public float Angle
-	{
-		get { return angle; }
-		set { angle = value; }
-	}
-
-	public float Speed
-	{
-		get { return speed; }
-		set { speed = value; }
-	}
-
-	public float Mass
-	{
-		get { return mass; }
-		set { mass = value; }
-	}
-
-	public float Strength
-	{
-		get { return strength; }
-		set { strength = value; }
-	}
-
-	public float Size
-	{
-		get { return size; }
-		set { size = value; }
-	}
-
-	public float Agression
-	{
-		get { return agression; }
-		set { agression = value; }
-	}
-
-	public float TurnSpeed
-	{
-		get { return turnSpeed; }
-		set { turnSpeed = value; }
-	}
-
-	public float Bounciness
-	{
-		get { return bounciness; }
-		set { bounciness = value; }
-	}
-
-	public int Shape
-	{
-		get { return shape; }
-		set { shape = value; }
-	}
-
-	public float Fear
-	{
-		get { return fear; }
-		set { fear = value; }
-	}
-	
-	
+        if (count > 120)
+        {
+            count = 0;
+            GameObject[] gos = GameObject.FindGameObjectsWithTag("team1");
+            var dist = 10000f;
+            for (var i=0;i< gos.Length;i++)
+            {
+                heading = gos[i].transform.position - transform.position;
+                distance = heading.magnitude;
+                if (distance < dist)
+                {
+                    dist = distance;
+                    target = gos[i];
+                }
+            }
+            //target = GameObject.Find("BasicBoid");
+            heading = target.transform.position - transform.position;
+            distance = heading.magnitude;
+            direction = heading / distance;
+            rbody.AddForce(direction * 15, ForceMode.Impulse);
+        }
+    }
 	
 	
 }
