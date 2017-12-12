8eb2a322e6a1bd8a483ef9945ababe81619e3d51
Ethan Schutzman <ehschutzman@wpi.edu>
2017-12-12 09:44:52 -0500

    Changing computers

---
+++ Assets/Scripts/Breeding.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breeding
+{
+	private int numberOfBoidsInFlock;
+	
+	
+	float separation;
+	float flocking;
+	float cohesion;
+	float direction;
+	float COH_NEIGHBORDIST;  //radius for boids to be cohesive with
+	float ALI_NEIGHBORDIST;  //radius for boids to align with
+	float SEP_DESIREDDIST;   //desired separation distance
+	float angle;
+	
+	float mass;
+	float strength;
+	float size;
+	float agression;
+	float turnSpeed;
+	float bounciness;
+	int shape;
+	float fear;
+
+
+
+	
+	
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
