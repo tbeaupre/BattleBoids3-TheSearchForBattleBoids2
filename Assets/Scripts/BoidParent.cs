b1694c563d25ada80042824971212bfa12f4942d
Eskibro830 <nhcarter@wpi.edu>
2017-12-13 14:06:44 -0500

    updated boids

---
+++ Assets/Scripts/BoidParent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidParent : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject boid;
+    public string enemy;
+    public Vector3 center = new Vector3(0, 0, 0);
+    public int count = 0;
+    public int count2 = 0;
+
+    public Rigidbody rbody;
+
+    //stats
+    public float speed;
+    public float mass;
+    public float size_x;
+    public float size_y;
+    public float size_z;
+    public float push_strength;
+    public float push_delay;
+    public float jump_strength;
+    public float jump_delay;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //speed = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void push()
+    {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag(enemy);
+        var dist = 10000f;
+        Vector3 heading;
+        Vector3 direction;
+        float distance;
+        for (var i = 0; i < gos.Length; i++)
+        {
+            heading = gos[i].transform.position - transform.position;
+            distance = heading.magnitude;
+            if (distance < dist)
+            {
+                dist = distance;
+                target = gos[i];
+            }
+        }
+        heading = target.transform.position - transform.position;
+        distance = heading.magnitude;
+        direction = heading / distance;
+        rbody.AddForce(direction * push_strength, ForceMode.Impulse);
+    }
+
+    public void jump() {
+        rbody.AddForce(new Vector3(0, 1, 0) * jump_strength, ForceMode.Impulse);
+    }
+
+}
--- Assets/Scripts/Boid_enemy.cs
+++ Assets/Scripts/Boid_enemy.cs
@@ -1,35 +1,40 @@
 ﻿using UnityEngine;
 
-public class BasicBoid : BoidParent {
-		
-	// Use this for initialization
-	void Start () {
+public class Boid_enemy : BoidParent
+{
+
+    // Use this for initialization
+    void Start()
+    {
         rbody = GetComponent<Rigidbody>();
         boid = GameObject.Find("BasicBoid");
-        enemy = "team2";
+        enemy = "team1";
 
-		speed = 2000f;
+        speed = 2000f;
         mass = 2;
-        size_x = 1;
-        size_y = 1;
-        size_z = 1;
+        size_x = 1f;
+        size_y = 1f;
+        size_z = 1.2f;
         push_strength = 20;
         push_delay = 40;
-        jump_strength = 60;
-        jump_delay = 100;
+        jump_strength = 0;
+        jump_delay = 10000;
+
+        transform.localScale = new Vector3(size_x,size_y,size_z);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         float step = speed * Time.deltaTime;
-		var heading = center - transform.position;
-		var distance = heading.magnitude;
-		var direction = heading / distance;
+        var heading = center - transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
 
         if (distance > 2) {
             direction = heading / distance;
             rbody.AddForce(direction * step);
-        }
+        } 
 
         if (count > push_delay && distance < 10) {
             count = 0;
