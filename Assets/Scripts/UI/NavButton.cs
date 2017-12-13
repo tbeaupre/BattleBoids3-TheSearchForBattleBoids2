c2bdc848992bdb1984c9929758a29c1598ff796c
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-12-13 14:21:43 -0500

    New Button System

---
+++ Assets/Scripts/UI/NavButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+	public class NavButton : Button
+	{
+		public GameObject target;
+		public GameObject source;
+
+		public override void OnClick()
+		{
+			source.SetActive(false);
+			target.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/SceneButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+	public class SceneButton : Button
+	{
+		[SerializeField] private string target;
+
+		public override void OnClick()
+		{
+			SceneManager.LoadScene(target);
+		}
+	}
+}
